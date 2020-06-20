ed35b9bb152f30f64dad6b4fc599de9a070d484d
VV <victor@gmail.com>
2020-06-20 18:07:07 +0300

    Updates

---
+++ precache-manifest.2727f2ebcc13683eaf21585244cc76af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a39f7991d84b1ec04b597fd85ba23296",
+    "url": "/app-react-todo/index.html"
+  },
+  {
+    "revision": "362e9cf02cc8662a1679",
+    "url": "/app-react-todo/static/css/main.ca10cc11.chunk.css"
+  },
+  {
+    "revision": "be19641d3fe78d35223f",
+    "url": "/app-react-todo/static/js/2.ef8b4283.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/app-react-todo/static/js/2.ef8b4283.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "362e9cf02cc8662a1679",
+    "url": "/app-react-todo/static/js/main.d202fcdd.chunk.js"
+  },
+  {
+    "revision": "c7ecf051ea8a376f573e",
+    "url": "/app-react-todo/static/js/runtime-main.93cb0fc2.js"
+  }
+]);
